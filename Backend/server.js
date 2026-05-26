6921773438e5e2a3bad0cedecebbc4e136163d03
anchalll02 <anchalmaurya0206@gmail.com>
2026-05-26 13:03:17 +0530

    fix cors

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:"https://united-gpt-pi.vercel.app/"
+}));
 
 app.use("/api", chatRoutes);
 
